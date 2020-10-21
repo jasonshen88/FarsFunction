e83a236b6cff1cd37d71913233fdb001ff4d871b
HansKH2 <73219942+HansKH2@users.noreply.github.com>
2020-10-21 09:57:03 +0200

    Add files via upload

---
+++ tests/testthat/test_fars_functions.R
@@ -0,0 +1,21 @@
+## test file for fars_functions.R file
+
+library(testthat)
+f<-file(system.file("R", "fars_functions.R", package = "farsfunctions"), open = "r")
+source(f)
+close(f)
+
+test_that("make_filename_string", {
+    temp <- make_filename(2015)
+    l <- nchar(temp)
+    expect_equal(l, 21)
+    expect_is(temp, "character")
+})
+
+test_that("fars_read_years_list", {
+    expect_warning(fars_read_years(9999))
+    temp <- fars_read_years(c(2013, 2014, 9999))
+    n <- temp[[3]]
+    expect_is(temp, "list")
+    expect_identical(n, NULL)
+})
